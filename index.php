547fa644c3096b03fabb860e3f2f3e4219128fda
incesamsul <incesamsul89@gmail.com>
2021-12-01 18:48:24 +0800

    fix ui

--- index.php
+++ index.php
@@ -9,13 +9,16 @@
   <!-- Bootstrap CSS -->
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
   <link rel="stylesheet" href="style.css">
+  <link rel="stylesheet" href="bubble-chat.css">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" integrity="sha512-Fo3rlrZj/k7ujTnHg4CGR2D7kSs0v4LLanw2qksYuRlEzO+tcaEPQogQ0KaoGN26/zrn20ImR1DfuLWnOo7aBA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+
   <title>Hello, world!</title>
 </head>
 
 <body>
 
-  <div class="container-fluid m-0 p-0 content-wrapper">
+  <div class="content-wrapper">
     <div class="list-friend-segment">
       <div class="sidebar-header">
         <div class="profile-wrapper">
@@ -55,14 +58,94 @@
       </div>
     </div>
     <div class="chat-segment">
-      <div class="welcome-wrapper">
-        <img src="./img/profile.jpg" alt="" width="500">
-        test
+      <div class="sidebar-header no-border fixed ">
+        <div class="profile-wrapper">
+          <div class="img-profile">
+            <img src="./img/profile.jpg" alt="" class="profile-pict">
+            <span class="p-3">Zulkarnain</span>
+          </div>
+          <div class="action-wrapper">
+            <i></i>
+            <i></i>
+            <i class="fas fa-search"></i>
+            <i class="fas fa-ellipsis-v"></i>
+          </div>
+        </div>
       </div>
-
+      <div class="chat-content">
+        <div class="talk-bubble tri-right round left-top left">
+          <div class="talktext">
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci ipsam praesentium, deserunt aliquam necessitatibus a aperiam in, atque officiis autem temporibus dolorem excepturi ab possimus nam eveniet voluptas mollitia minus.</p>
+          </div>
+        </div>
+        <div class="talk-bubble tri-right round right-top right">
+          <div class="talktext">
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci ipsam praesentium, deserunt aliquam necessitatibus a aperiam in, atque officiis autem temporibus dolorem excepturi ab possimus nam eveniet voluptas mollitia minus.</p>
+          </div>
+        </div>
+        <div class="talk-bubble tri-right round left-top left">
+          <div class="talktext">
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci ipsam praesentium, deserunt aliquam necessitatibus a aperiam in, atque officiis autem temporibus dolorem excepturi ab possimus nam eveniet voluptas mollitia minus.</p>
+          </div>
+        </div>
+        <div class="talk-bubble tri-right round right-top right">
+          <div class="talktext">
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci ipsam praesentium, deserunt aliquam necessitatibus a aperiam in, atque officiis autem temporibus dolorem excepturi ab possimus nam eveniet voluptas mollitia minus.</p>
+          </div>
+        </div>
+        <div class="talk-bubble tri-right round left-top left">
+          <div class="talktext">
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci ipsam praesentium, deserunt aliquam necessitatibus a aperiam in, atque officiis autem temporibus dolorem excepturi ab possimus nam eveniet voluptas mollitia minus.</p>
+          </div>
+        </div>
+        <div class="talk-bubble tri-right round right-top right">
+          <div class="talktext">
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci ipsam praesentium, deserunt aliquam necessitatibus a aperiam in, atque officiis autem temporibus dolorem excepturi ab possimus nam eveniet voluptas mollitia minus.</p>
+          </div>
+        </div>
+        <div class="talk-bubble tri-right round left-top left">
+          <div class="talktext">
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci ipsam praesentium, deserunt aliquam necessitatibus a aperiam in, atque officiis autem temporibus dolorem excepturi ab possimus nam eveniet voluptas mollitia minus.</p>
+          </div>
+        </div>
+        <div class="talk-bubble tri-right round right-top right">
+          <div class="talktext">
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci ipsam praesentium, deserunt aliquam necessitatibus a aperiam in, atque officiis autem temporibus dolorem excepturi ab possimus nam eveniet voluptas mollitia minus.</p>
+          </div>
+        </div>
+        <div class="talk-bubble tri-right round left-top left">
+          <div class="talktext">
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci ipsam praesentium, deserunt aliquam necessitatibus a aperiam in, atque officiis autem temporibus dolorem excepturi ab possimus nam eveniet voluptas mollitia minus.</p>
+          </div>
+        </div>
+        <div class="talk-bubble tri-right round right-top right">
+          <div class="talktext">
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci ipsam praesentium, deserunt aliquam necessitatibus a aperiam in, atque officiis autem temporibus dolorem excepturi ab possimus nam eveniet voluptas mollitia minus.</p>
+          </div>
+        </div>
+      </div>
+      <div class="chat-input-wrapper ">
+        <i class="far fa-laugh-squint"></i>
+        <i class="fas fa-paperclip"></i>
+        <input type="text" class="round-input" placeholder="ketik pesan">
+        <i class="fas fa-microphone"></i>
+      </div>
     </div>
   </div>
-
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+  <script>
+    $(document).ready(function() {
+      $('.content-wrapper').css('margin', '0px auto')
+    })
+    $(window).resize(function() {
+      let windowSize = $(window).width();
+      if (windowSize > 1364.2) {
+        $('.content-wrapper').css('margin', '10px auto')
+      } else {
+        $('.content-wrapper').css('margin', '0px auto')
+      }
+    }).resize();
+  </script>
 </body>
 
 </html>
---
+++ welcome.php
@@ -0,0 +1,9 @@
+<div class="welcome-wrapper">
+    <img src="./img/profile.jpg" alt="" class="round-image img-230px">
+    <p class="welcome-text mt-4">Pastikan telepon anda terhubung ke internet</p>
+
+    <p class="text-small">Whatsapp melakukan koneksi ke telepon anda untuk menyinkronkan pesan. untuk mengurangi penggunaan data, sambungkan telepon anda ke wifi</p>
+    <hr class="divider">
+    <p class="text-small"><i class="fas fa-laptop"></i> lakukan panggilan dari desktop dengan whastapp untuk windows. dapatkan disini</p>
+
+</div>
